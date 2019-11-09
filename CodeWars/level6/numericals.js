251622e4d3b00e10be816f1d238dd28f1d96be42
cule219 <culaficstefan@gmail.com>
2019-11-09 11:36:42 -0500

    numericals

---
+++ CodeWars/level6/numericals.js
@@ -0,0 +1,12 @@
+function numericals(s){
+    //   return [...s].map((x, i) => 
+    //   s.substring(0, i+1).match(new RegExp(`[${x.replace(/[.*+?^${}()|[\]\\]/g, '\\$&')}]`, `g`)).length
+    //   ).join('');
+      let obj = {};
+      let res = '';
+      for(let i = 0; i < s.length; i++) {
+        obj[s[i]] ? obj[s[i]]++ : obj[s[i]] = 1;
+        res += obj[s[i]];
+      }
+      return res;
+}
