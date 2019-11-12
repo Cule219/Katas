f3d63d95b0c8150f786d24c9351e49bb999ff190
cule219 <culaficstefan@gmail.com>
2019-11-12 15:50:47 -0500

    tribonacci

--- CodeWars/level6/tribonacci.js
+++ CodeWars/level6/tribonacci.js
@@ -1,7 +1,6 @@
 function tribonacci(signature,n){
     //your code here
-    let i = n;
-    while(i > 3) {
+    while(signature.length > 3) {
       signature.push(signature.slice(-3).reduce((a,v)=>a+v));
       i--;
     }
