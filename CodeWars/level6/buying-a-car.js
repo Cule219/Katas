0fb61f18714790f7babcfdef2d38155fb99d7f39
cule219 <culaficstefan@gmail.com>
2019-11-25 20:47:49 -0500

    buying a car

---
+++ CodeWars/level6/buying-a-car.js
@@ -0,0 +1,9 @@
+function nbMonths(sPO, sPN, sPM, pLPM, m=0){
+    return sPO+sPM*m>sPN?
+       [m,Math.round(sPO+sPM*m-sPN)]
+      :
+      (pLPM = m%2&&m!=0?pLPM+0.5:pLPM,
+      sPO = sPO-(sPO*pLPM/100),
+      sPN = sPN-(sPN*pLPM/100),
+      nbMonths(sPO, sPN, sPM, pLPM, ++m))
+  }
