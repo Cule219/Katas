d923f485918f796c9255e5870228cd0be9f59043
cule219 <culaficstefan@gmail.com>
2020-01-06 16:10:12 -0500

    clocky-mr-clock-face

--- CodeWars/level5/count-ip-addresses.js
+++ CodeWars/level5/count-ip-addresses.js
@@ -1,5 +1,5 @@
-function ipsBetween(start, end){
-    //TODO
-    end   = end.split('.');
-    return start.split('.').map((x, i) => end[i]-x).reduce((acc,val,ind)=>acc+=val*256**(3-ind),0);
-  }
+function ipsBetween (start, end){
+	// TODO
+	end = end.split('.');
+	return start.split('.').map((x, i) => end[i] - x).reduce((acc, val, ind) => (acc += val * 256 ** (3 - ind)), 0);
+}
--- CodeWars/level6/longest-alphabetical-substring.js
+++ CodeWars/level6/longest-alphabetical-substring.js
@@ -1,6 +1,7 @@
-function longest(str) {
-    return [...str].reduce((a,v,i) => i > 0 && v >= str[i-1] ? 
-    (a[a.length-1] += v, a) : (a.push(v),a), [])
-    .reduce((a,v) => a.length < v.length ? 
-    (a = v, a) : a, '');
-  }
+function longest (str){
+	return [
+		...str
+	]
+		.reduce((a, v, i) => (i > 0 && v >= str[i - 1] ? ((a[a.length - 1] += v), a) : (a.push(v), a)), [])
+		.reduce((a, v) => (a.length < v.length ? ((a = v), a) : a), '');
+}
