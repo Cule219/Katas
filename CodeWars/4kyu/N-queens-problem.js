ef674c67671061f3aad79cb62cb58cbb7773a773
Cule219 <culaficstefan@gmail.com>
2021-01-08 22:34:14 -0500

    final

--- CodeWars/4kyu/N-queens-problem.js
+++ CodeWars/4kyu/N-queens-problem.js
@@ -1,37 +1,39 @@
-function nQueen(n) {
-  const board = (this.board = Array.from({ length: n }, () =>
-    new Array(n).fill(0)
-  ));
-  return solveNQueen(board, 0, n) ? board.map((r) => r.indexOf(1)) : [];
-}
-
-function solveNQueen(board, col, n) {
-  if (col >= n) {
-    return true;
+class GFG {
+  constructor(n) {
+    this.N = n;
+    this.ld = [];
+    this.rd = [];
+    this.cl = [];
   }
-  for (let i = 0; i < n; i++) {
-    if (setQueen(board, i, col, n)) {
-      board[i][col] = 1;
-      if (solveNQueen(board, col + 1, n) === true) {
-        return true;
+
+  solveNQueen = (board, col) => {
+    if (col >= this.N) return true;
+    for (let i = 0; i < this.N; i++) {
+      if (
+        this.ld[i - col + this.N - 1] != 1 &&
+        this.rd[i + col] != 1 &&
+        this.cl[i] != 1
+      ) {
+        board[i][col] = 1;
+        this.ld[i - col + this.N - 1] = 1;
+        this.rd[i + col] = 1;
+        this.cl[i] = 1;
+
+        if (this.solveNQueen(board, col + 1)) return true;
+
+        board[i][col] = 0; // BACKTRACK
+        this.ld[i - col + this.N - 1] = 0;
+        this.rd[i + col] = 0;
+        this.cl[i] = 0;
       }
     }
-    board[i][col] = 0;
-  }
-  return false;
+    return false;
+  };
+  nQueen = () => {
+    const board = (this.board = Array.from({ length: this.N }, () =>
+      new Array(this.N).fill(0)
+    ));
+    return this.solveNQueen(board, 0) ? board.map((r) => r.indexOf(1)) : [];
+  };
 }
-
-function setQueen(board, row, col, n) {
-  for (let i = 0; i < col; i++)
-    if (board[row][i] === 1) {
-      return false;
-    }
-
-  for (let i = row, j = col; i >= 0 && j >= 0; i--, j--)
-    if (board[i][j] === 1) return false;
-  for (let i = row, j = col; i < n && j >= 0; i++, j--)
-    if (board[i][j] === 1) return false;
-  return true;
-}
-
-console.log("this", nQueen(50));
+console.log("this", new GFG(55).nQueen());
